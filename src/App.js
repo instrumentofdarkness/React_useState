8c1397939fced0ca076a34f1fa17a9d91b2bd51c
instrumentofdarkness <127889101+instrumentofdarkness@users.noreply.github.com>
2023-08-28 08:51:24 +0200

    added the hook to the counter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
+import Counter from "./components/Counter";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <h1>React useState hook</h1>
+    <Counter />
+    </div>;
 }
 
 export default App;
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,5 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Counter() {
-  return <div>Counter</div>;
+  // useState - React hook
+  // result - state variable; 0 / 1 / 2, etc. - initial value
+  // setResult - function to update the state variable, action to update the state / trigger the re-render of the component / allow us to chanage the value of the state variable
+  // useState() - initial state
+  const [result, setResult] = useState(10);
+
+  let value = 1;
+  // logic
+  function increment() {
+    value += 1;
+    // update the state variable - 0 -> 1 -> 2 -> 3 -> 4 -> 5
+    setResult(result + 1);
+
+  }
+  function decrement() {
+    value -= 1;
+  }
+  return (
+    <div>
+      <h1>Counter</h1>
+      <button onClick={increment}>+</button>
+      <p>Value here: {value}</p>
+      <p>Value from state: {result}</p>
+      <button onClick={decrement}>-</button>
+      <p>Value here: {value}</p>
+      <p>Value from state: {result}</p>
+    </div>
+  );
 }
